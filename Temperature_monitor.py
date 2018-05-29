5347db3b56d0581ed3fe9be7d77b3fa1f074847e
Laura Matic <lauramatic@gmail.com>
2018-05-29 22:19:45 +0200

    Specified the ser.write and serial port, split temperature line

--- Temperature_monitor.py
+++ Temperature_monitor.py
@@ -1,11 +1,16 @@
-# import serial library
-import serial
-ser = serial.Serial('/dev/ttyUSB0')         # open serial port
-print(ser.name)                             # check which port was really used
+import serial, datetime
 
-ser.write("sudo minicom -D /dev/ttyUSB0")   # open minicom
-ser.write()                                 # enter password
-ser.write("status_get")                     # gets the status of the device
-read_data = ser.read(13)                    # read line 13 - PV2_MIN_TEMPERATURE
-# response = ser.readline()
-print("Data received : " + read_data)
+
+ser = serial.Serial("/dev/ttyUSB0", "115200")         # open serial port
+print(ser.name)                                       # check which port was really used
+
+ser.write("status_get\r\n")                           # input command status_get
+
+read_line = ser.readline()
+while("PV2_EPD_TEMP_SENSOR" not in read_line):        # read until it comes across the temperature
+    read_line = ser.readline()
+    if "PV2_EPD_TEMP_SENSOR" in read_line:
+        temp = read_line.split(":")[1]
+        print("Temperature at {1} was {0} degrees celsius".format(temp.strip(), datetime.datetime.today().isoformat()))
+        return(temp.strip(),datetime.datetime.today().isoformat())
+ser.close()
