070e04e4b093ebd42cfb0f7b9ed2694d80f3fe19
Laura Matic <lauramatic@gmail.com>
2018-05-30 19:42:54 +0200

    Created a class and functions.

--- Temperature_monitor.py
+++ Temperature_monitor.py
@@ -1,16 +1,39 @@
-import serial, datetime
+import serial
+import datetime
+
+BAUDRATE = "115200"
+
+class Visionect:
+    def __init__(self, port, rate):
+        self.port = port
+        self.rate = rate
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+    def get_temperature(self):
+        ser = serial.Serial(self.port, self.rate)             # open serial port
+        print(ser.name)                                       # check which port was really used
+
+        ser.write("status_get\r\n")                           # input command status_get
+
+        read_line = ser.readline()
+        while("PV2_EPD_TEMP_SENSOR" not in read_line):        # read until it comes across the temperature
+            read_line = ser.readline()
 
 
-ser = serial.Serial("/dev/ttyUSB0", "115200")         # open serial port
-print(ser.name)                                       # check which port was really used
-
-ser.write("status_get\r\n")                           # input command status_get
-
-read_line = ser.readline()
-while("PV2_EPD_TEMP_SENSOR" not in read_line):        # read until it comes across the temperature
-    read_line = ser.readline()
-    if "PV2_EPD_TEMP_SENSOR" in read_line:
-        temp = read_line.split(":")[1]
+        temp = read_line.split(":")[1]                        # split to get just the temperature
         print("Temperature at {1} was {0} degrees celsius".format(temp.strip(), datetime.datetime.today().isoformat()))
-        return(temp.strip(),datetime.datetime.today().isoformat())
-ser.close()
+
+        ser.close()
+        return temp.strip(), datetime.datetime.today().isoformat()
+
+d1 = Visionect("/dev/ttyUSB0", BAUDRATE)
+print d1.get_temperature()
+
+# define class Reading with temperature and date as their attributes
+# read temperature for 5 minutes and save values to list
+# result: [temp1, temp2, temp3,...]
